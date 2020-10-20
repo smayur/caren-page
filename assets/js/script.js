c000b617880d58f0d38539d078f049eb3e85b8d7
mayurS <mayurs@prdxn.com>
2020-10-20 23:19:03 +0530

    add classes to burger-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,7 @@
-/* Author: 
+var menu = document.querySelector('.menu');
+var burger = document.querySelector('.burger');
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+burger.addEventListener("click", function() {
+    menu.classList.add("show");
+    console.log(menu);
+});
