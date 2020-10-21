cb1c890e0a6e1eb24d6956f505ef0316ba85a41e
mayurS <mayurs@prdxn.com>
2020-10-21 12:58:32 +0530

    commented js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-var menu = document.querySelector('.menu');
-var showMenu = document.querySelector('.show-menu');
-var burger = document.querySelector('.burger');
+// var menu = document.querySelector('.menu');
+// var showMenu = document.querySelector('.show-menu');
+// var burger = document.querySelector('.burger');
 
-burger.addEventListener("click", function() {
+// burger.addEventListener("click", function() {
     
-    if(showMenu.length > 0) {
-        menu.classList.add("hide-menu");
-        menu.classList.remove("show-menu");
-    }else{
-        menu.classList.add("show-menu");
-        menu.classList.remove("hide-menu");
-    }
-    console.log(menu);
-});
+//     if(showMenu.length > 0) {
+//         menu.classList.add("hide-menu");
+//         menu.classList.remove("show-menu");
+//     }else{
+//         menu.classList.add("show-menu");
+//         menu.classList.remove("hide-menu");
+//     }
+//     console.log(menu);
+// });
